fd0ef2a43af2ce0097fe82788225ec1cbb0a3267
ramanjot1988 <ramanjot.kaur.88@gmail.com>
2019-08-11 12:09:30 -0400

    initial waterLoo commit

---
+++ SecondProject08Aug/src/test/java/Aug/SecondProject08Aug/WaterLoo.java
@@ -0,0 +1,27 @@
+package Aug.SecondProject08Aug;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class WaterLoo {
+	@Test
+	public void waterLoo() {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\SeleniumJars\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		// It will open URL in Browser
+		driver.get("https://uwaterloo.ca");
+		System.out.println(driver.findElement(By.xpath("(//span[@class='teaser'])[3]")).getText());
+
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+
+}
